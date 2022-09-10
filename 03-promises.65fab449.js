e89aacd46db08bf90424ea472154ced59d35f741
EvgenyShokotko <103266879+EvgenyShokotko@users.noreply.github.com>
2022-09-10 19:39:23 +0000

    Deploying to gh-pages from @ EvgenyShokotko/goit-js-hw-09@6a3fb84bbb3b005634574f173fd9522aab1aacff 🚀

--- 03-promises.65fab449.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector("button");console.log(o);var l=document.querySelectorAll("input");console.log(l);var e=l[0];console.log(e);var n=l[1];console.log(n);var c=l[2];console.log(c)}();
-//# sourceMappingURL=03-promises.65fab449.js.map
